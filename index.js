84783342dda5c536bf1048d94e8747528427f5a4
Thomas Tuts <thomas@thomastuts.com>
2016-08-05 16:46:22 +0200

    Add solution code (#2)
    * Add content for first draft and accompanying tests
    
    * Add solution code
    
    * Update tests
    
    It seems like the counts were off -- the original array has 9 episodes,
    and bingeWatch() should run 10 times including the finale
    
    * Get tests passing in browser and in test environment
    
    * Add note about arrow functoin for bingeWatch()

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much.";
+const episodes = [
+  { id: 's06e01', title: 'The Red Woman' },
+  { id: 's06e02', title: 'Home' },
+  { id: 's06e03', title: 'Oathbreaker' },
+  { id: 's06e04', title: 'Book of the Stranger' },
+  { id: 's06e05', title: 'The Door' },
+  { id: 's06e06', title: 'Blood of My Blood' },
+  { id: 's06e07', title: 'The Broken Man' },
+  { id: 's06e08', title: 'No One' },
+  { id: 's06e09', title: 'Battle of the Bastards' },
+];
+
+const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+const addToPlaylist = (playlist, episode) => [...playlist, episode];
+const removeFromPlaylist = (playlist, episode) => playlist.filter(e => e.id !== episode.id);
+const getNextEpisodeInPlaylist = playlist => playlist[0];
+
+// :( We need to use `function` so that we can spy on
+// it. This is bad testing practice in the real world,
+// but maybe it's okay in the contrived world of Learn?
+// In the real world, `const bingeWatch = playlist => { ... }`
+// is just fine.
+function bingeWatch(playlist) {
+  if (playlist.length === 0) {
+    return 'Please let there be more!';
+  }
+
+  const nextEpisode = getNextEpisodeInPlaylist(playlist);
+  return bingeWatch(removeFromPlaylist(playlist, nextEpisode));
+};
--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,42 @@
+describe('Pure functions', function() {
+  describe('getNextEpisodeInPlaylist()', function () {
+    it('should return the next episode in a playlist', function() {
+      const nextEpisode = getNextEpisodeInPlaylist(episodes);
+      expect(nextEpisode).toEqual(episodes[0]);
+    });
+  });
 
+  describe('addToPlaylist()', function () {
+    it('should add a given episode to a given playlist', function() {
+      const updatedPlaylist = addToPlaylist(episodes, finaleEpisode);
+      expect(updatedPlaylist.length).toEqual(episodes.length + 1);
+    });
 
-describe('index', function() {
-  it('runs', function() {
-    expect(true).toEqual(true)
-  })
-})
+    it('should not modify the original episodes array', function() {
+      addToPlaylist(episodes, finaleEpisode);
+      expect(episodes.length).toEqual(9);
+    });
+  });
+
+  describe('removeFromPlaylist()', function () {
+    it('should remove a given episode from a given playlist', function() {
+      const updatedPlaylist = removeFromPlaylist(episodes, episodes[0]);
+      expect(updatedPlaylist.length).toEqual(episodes.length - 1);
+    });
+
+    it('should not modify the original episodes array', function() {
+      removeFromPlaylist(episodes, episodes[0]);
+      expect(episodes.length).toEqual(9);
+    });
+  });
+
+  describe('bingeWatch()', function () {
+    it('should watch all 10 episodes', function() {
+      const updatedPlaylist = addToPlaylist(episodes, finaleEpisode);
+      const spy = sinon.spy(window, 'bingeWatch')
+      const result = window.bingeWatch(updatedPlaylist);
+      expect(spy.callCount).toEqual(11);
+      expect(result).toEqual('Please let there be more!');
+    });
+  });
+});
