47ab1221921cc1b5b6e082eb36b00f0f35798dfa
briecoyle <brie.coyle@gmail.com>
2017-09-15 15:51:33 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ const episodes = [
 ];
 
 const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+function addToPlaylist(playlist, episode) {
+  return playlist.concat([episode])
+}
+
+function removeFromPlaylist(playlist, episode) {
+  return playlist.filter(function(listedEpisode) {
+    return listedEpisode.id != episode.id
+  })
+}
+
+function getNextEpisodeInPlaylist(playlist) {
+  return playlist[0]
+}
+
+function bingeWatch(playlist) {
+  if (playlist.length === 0) {
+    return 'Please let there be more!';
+  }
+
+  let this_episode = getNextEpisodeInPlaylist(playlist);
+  return bingeWatch(removeFromPlaylist(playlist, this_episode));
+}
