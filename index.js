7a3401624cb3119c07ad31d90540532796e5f0ae
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-07-21 02:04:36 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,33 @@ const episodes = [
 ];
 
 const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+const getNextEpisodeInPlaylist = (series) => series[0]
+
+// function* series() {
+//   yield* episodes;
+// }
+
+// var gOt = series()
+
+// function getNextEpisodeInPlaylist() {
+//   return gOt.next().value
+// }
+
+const addToPlaylist = (series, element) => {
+  return [...series, element]
+}
+
+const removeFromPlaylist = (series, element) => {
+  return series.filter(el => el != element)
+}
+
+function bingeWatch(playlist) {
+  if (playlist.length > 0) {
+    let first = getNextEpisodeInPlaylist(playlist);
+    let removed = removeFromPlaylist(playlist, first);
+    return bingeWatch(removed);
+  }
+  
+  return 'Please let there be more!';
+}
