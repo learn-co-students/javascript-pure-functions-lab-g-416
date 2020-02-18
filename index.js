c553af39ad782739678bf904321f60916da0f2eb
markpnoi26 <markpnoi26@gmail.com>
2020-02-17 20:39:22 -0800

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,46 @@ const episodes = [
 ];
 
 const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+
+// function getNextEpisodeInPlaylist(playlist) {
+//   return playlist[0]
+// }
+//
+// function addToPlaylist(playlist, episode) {
+//   let newPlaylist = Object.assign([], playlist, episode)
+//   return newPlaylist
+// }
+//
+// function removeFromPlaylist(playlist, episode) {
+//   let newPlaylist = playlist.filter(epi => epi.id !== episode.id)
+//   return newPlaylist
+// }
+
+// function bingeWatch(playlist) {
+//   let nextEpisode = getNextEpisodeInPlaylist(playlist)
+//   let newPlaylist = removeFromPlaylist(playlist, nextEpisode)
+//   if (playlist.length > 0) {
+//     return bingeWatch(newPlaylist)
+//   } else {
+//     return "Please let there be more!"
+//   }
+// }
+
+const addToPlaylist = (playlist, episode) => [...playlist, episode];
+const removeFromPlaylist = (playlist, episode) => playlist.filter(e => e.id !== episode.id);
+const getNextEpisodeInPlaylist = playlist => playlist[0];
+
+// :( We need to use `function` so that we can spy on
+// it. This is bad testing practice in the real world,
+// but maybe it's okay in the contrived world of Learn?
+// In the real world, `const bingeWatch = playlist => { ... }`
+// is just fine.
+function bingeWatch(playlist) {
+  if (playlist.length === 0) {
+    return 'Please let there be more!';
+  }
+
+  const nextEpisode = getNextEpisodeInPlaylist(playlist);
+  return bingeWatch(removeFromPlaylist(playlist, nextEpisode));
+};
